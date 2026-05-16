979ebb67e2ce12bbf779b134570551a083686905
LusterSourav <282348889+LusterSourav@users.noreply.github.com>
2026-05-16 20:14:10 +0530

    Nibble type system (#9)
    Implements Nibble newtype, NibbleIterator, and HP padding. Closes #7.

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,20 @@
 #![no_std]
 #![cfg_attr(feature = "runtime", feature(alloc_error_handler))]
 
+pub mod nibble;
 pub mod ops;
 pub mod types;
 
 #[cfg(feature = "runtime")]
 pub mod lang_items;
 
+pub use nibble::Nibble;
+pub use nibble::NibbleIterator;
+pub use nibble::NibblePathPacked;
+pub use nibble::encode_nibble_path_padded;
+pub use nibble::high_nibble;
+pub use nibble::low_nibble;
+pub use nibble::nibbles_to_byte;
 pub use types::U256;
 pub use types::U256_MAX;
 pub use types::U512;
